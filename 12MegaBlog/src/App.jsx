d2154d68be7c744e96b5c9818ad18c74ee3e8317
dnyaneshwar469 <dnyaneshwar.londhepatil@anuyat.com>
2023-12-06 17:08:24 +0530

    use refe implimentation

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -8,32 +8,32 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer'
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
+    const [loading, setLoading] = useState(true);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    authService.getCurrentUser()
-      .then((userData) => {
-        if (userData) {
-          dispatch(login({ userData }))
-        } else {
-          dispatch(logout())
-        }
-      })
-      .finally(() => setLoading(false))
-  }, [])
+    useEffect(() => {
+        authService.getCurrentUser()
+            .then((userData) => {
+                if (userData) {
+                    dispatch(login({ userData }))
+                } else {
+                    dispatch(logout())
+                }
+            })
+            .finally(() => setLoading(false))
+    }, [])
 
-  return !loading ? (
-    <div className='min-h-screen flex flex-wrap bg-gray-400'>
-      <div className='w-full block'>
-        <Header />
-        <main>
-          {/* <Outlet /> */}
-        </main>
-        <Footer />
-      </div>
-    </div>
-  ) : null
+    return !loading ? (
+        <div className='min-h-screen flex flex-wrap bg-gray-400'>
+            <div className='w-full block'>
+                <Header />
+                <main>
+                    {/* <Outlet /> */}
+                </main>
+                <Footer />
+            </div>
+        </div>
+    ) : null
 }
 
 export default App
--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
 import Footer from "./Footer/Footer";
+import container from "./container/Container";
+import Logo from "./Logo";
 
-export {Header, Footer}
+export {Header, Footer, container, Logo, LogoutBtn}
